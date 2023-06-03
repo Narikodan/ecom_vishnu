6db4172e9d5476277c6aae8dd945829f3d26c03d
vishnu narikodan <vishnunarikodan@gmail.com>
2023-06-03 12:42:23 +0530

    added search functionality in homepage

--- myapp/views.py
+++ myapp/views.py
@@ -11,6 +11,9 @@ from django.http import JsonResponse
 def index(request):
     success_message = messages.get_messages(request)
     products = Product.objects.all()
+    item_name = request.GET.get('item_name')
+    if item_name != '' and item_name is not None:
+        products = products.filter(name__icontains=item_name)
     user = request.user
     first_name = ''
     if user.is_authenticated:
